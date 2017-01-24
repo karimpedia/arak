eab3423646080d1de43b2d045f9f6fe2b76f88e7
Karim Barsim <karim.barsim@iss.uni-stuttgart.de>
2017-01-24 10:19:19 +0100

    minor

--- arak/exp/gan/MultiGenSingleDisc_MNIST_keras_SingleEpochPerModel.py
+++ arak/exp/gan/MultiGenSingleDisc_MNIST_keras_SingleEpochPerModel.py
@@ -57,6 +57,7 @@ DISCRIMINATOR_OPTIMIZER = Adam(1e-3)
 REPRESENTATION_DIMS = (IMG_ROWS, IMG_COLS, 1)
 NUM_EPOCHS = 300
 BATCH_SIZE = 100
+NUM_GENERATORS = 2
 # -----------------------------------------------------------------------------
 # =============================================================================
 
@@ -77,6 +78,7 @@ def set_trainable(model, trainable):
         x.trainable = trainable
     return model
 
+
 def freeze_model(model):
     return set_trainable(model, False)
 
@@ -92,35 +94,32 @@ def is_frozen(model):
 def build_generator():
     print TS('Building the generator ...')
     inLayer = Input(shape=(GENERATOR_CODE_DIM,))
-    H = Dense(14 * 14 * 128, init='glorot_normal')(inLayer)
+    H = Dense(100, init='he_normal')(inLayer)
+    H = Activation('sigmoid')(H)
+    H = Dense(7*7*128, init='he_normal')(inLayer)
     H = BatchNormalization(mode=2)(H)
-    H = Activation('relu')(H)
-    H = Reshape([14, 14, 128])(H)
+    H = Activation('sigmoid')(H)
+    H = Reshape([7, 7, 128])(H)
     H = UpSampling2D(size=(2, 2), dim_ordering='tf')(H)
-    H = Convolution2D(nb_row=3,
-                      nb_col=3,
-                      nb_filter=128,
+    H = Convolution2D(nb_row=5,
+                      nb_col=5,
+                      nb_filter=64,
                       border_mode='same',
                       dim_ordering='tf',
-                      init='glorot_uniform')(H)
+                      init='he_normal')(H)
     H = BatchNormalization(mode=2)(H)
-    H = Activation('relu')(H)
-    H = Convolution2D(nb_filter=64,
-                      nb_row=3,
-                      nb_col=3,
+    H = Activation('sigmoid')(H)
+    H = UpSampling2D(size=(2, 2), dim_ordering='tf')(H)
+    H = Convolution2D(nb_row=5,
+                      nb_col=5,
+                      nb_filter=1,
                       border_mode='same',
                       dim_ordering='tf',
-                      init='glorot_uniform')(H)
-    H = BatchNormalization(mode=2)(H)
-    H = Activation('relu')(H)
-    H = Convolution2D(nb_filter=1,
-                      nb_row=1,
-                      nb_col=1,
-                      dim_ordering='tf',
-                      border_mode='same', init='glorot_uniform')(H)
+                      init='he_normal')(H)
     outLayer = Activation('sigmoid')(H)
     model = Model(input=inLayer, output=outLayer)
-    model.compile(loss='binary_crossentropy', optimizer=GENERATOR_OPTIMIZER)
+    model.compile(loss='binary_crossentropy',
+                  optimizer=GENERATOR_OPTIMIZER)
     model.summary()
     return model
 
@@ -148,7 +147,7 @@ def build_discriminator():
     H = Dense(256)(H)
     H = LeakyReLU(alpha=0.2)(H)
     H = Dropout(0.33)(H)
-    outLayer = Dense(2, activation='softmax')(H)
+    outLayer = Dense(1 + NUM_GENERATORS, activation='softmax')(H)
     model = Model(input=inLayer, output=outLayer)
     model.compile(loss='binary_crossentropy',
                   optimizer=DISCRIMINATOR_OPTIMIZER)
@@ -168,46 +167,63 @@ def build_gan(gModel, dModel):
     return model
 
 
-def train_disc_on_batch(gModel, dModel, ganModel, inReal):
+def train_disc_on_batch(gModels, dModel, ganModel, inReal):
     st = time.time()
     inGEN = \
         np.random.uniform(0., 1., size=(inReal.shape[0], GENERATOR_CODE_DIM))
-    inFake = gModel.predict(inGEN)
+    inFake = gModels[0].predict(inGEN)
+    refDISC = np.concatenate((np.zeros(inReal.shape[0]),
+                              np.ones(inFake.shape[0])), axis=0)
+    for x in range(1, len(gModels)):
+        inFake = np.concatenate((inFake, gModels[x].predict(inGEN)), axis=0)
+        refDISC = \
+            np.concatenate((refDISC,
+                            float(x + 1) * np.ones(inReal.shape[0])), axis=0)
     inDISC = np.concatenate((inFake, inReal), axis=0)
-    refDISC = np.concatenate((np.zeros(inFake.shape[0]),
-                              np.ones(inReal.shape[0])), axis=0)
-    refDISC = keras.utils.np_utils.to_categorical(refDISC, 2)
-    assert(np.array_equal(inFake.shape, inReal.shape))
-    assert(np.array_equal(refDISC.shape, np.array([inReal.shape[0]*2, 2])))
+    refDISC = keras.utils.np_utils.to_categorical(refDISC, 1+len(gModels))
+    assert(inFake.shape[0] == len(gModels) * inReal.shape[0])
+    assert(np.array_equal(inFake.shape[1:], inReal.shape[1:]))
+    assert(refDISC.shape[0] == inReal.shape[0] * (1 + len(gModels)))
+    assert(refDISC.shape[1] == (1 + len(gModels)))
     assert(np.amin(refDISC) == 0 and np.amax(refDISC) == 1)
-    assert(is_frozen(dModel))
+    assert(np.sum(refDISC) == refDISC.shape[0])
+    assert(np.all(np.sum(refDISC, axis=1)))
+    assert(np.array_equal(np.sum(refDISC, axis=0),
+                          np.ones(len(gModels) + 1) * inReal.shape[0]))
     unfreeze_model(dModel)
     outLoss = dModel.train_on_batch(inDISC, refDISC)
-    outDISC = dModel.predict(inDISC)
-    return outLoss, time.time() -st
+    return outLoss, time.time() - st
 
 
-def train_gen_on_batch(gModel, dModel, ganModel, batch_size):
+def train_gen_on_batch(gModel, dModel, ganModel, num_samples, num_gens):
     st = time.time()
-    inGAN = \
-        np.random.uniform(0., 1., size=(2 * batch_size, GENERATOR_CODE_DIM))
-    refGAN = np.ones(2 * batch_size)
-    refGAN = keras.utils.np_utils.to_categorical(refGAN, 2)
-    assert(np.array_equal(refGAN.shape, np.array([2 * batch_size, 2])))
+    inGAN = np.random.uniform(0., 1., size=(num_samples, GENERATOR_CODE_DIM))
+    refGAN = np.zeros(num_samples)
+    refGAN = keras.utils.np_utils.to_categorical(refGAN, num_gens+1)
+    assert(np.array_equal(refGAN.shape, np.array([num_samples, num_gens+1])))
     assert(np.amin(refGAN) == 0 and np.amax(refGAN) == 1)
-    assert(not is_frozen(dModel))
     freeze_model(dModel)
     outLoss = ganModel.train_on_batch(inGAN, refGAN)
-    return outLoss, time.time() -st
+    return outLoss, time.time() - st
 
 
-def train_gan_on_batch(gModel, dModel, ganModel, inReal):
-    dLoss, dTime = train_disc_on_batch(GEN, DSC, GAN, inReal)
-    gLoss, gTime = train_gen_on_batch(GEN, DSC, GAN, inReal.shape[0])
-    return dLoss, gLoss, dTime, gTime
+def train_gan_on_batch(gModels, dModel, ganModels, inReal):
+    dLosses, dTimes = [], []
+    for x, y in zip(gModels, ganModels):
+        dL, dT = train_disc_on_batch(gModels, dModel, ganModels, inReal)
+        dLosses.append(dL)
+        dTimes.append(dT)
+    gLosses, gTimes = [], []
+    for x, y in zip(gModels, ganModels):
+        gL, gT = train_gen_on_batch(x, dModel, y,
+                                    inReal.shape[0] * (len(gModels) + 1),
+                                    len(gModels))
+        gLosses.append(gL)
+        gTimes.append(gT)
+    return dLosses, gLosses, dTimes, gTimes
 
 
-def generate_and_save(gModel, dModel, ganModel, num_images, prefix):
+def generate_and_save(gModel, num_images, prefix):
     inGEN = np.random.uniform(0., 1., size=(num_images, GENERATOR_CODE_DIM))
     outGEN = np.squeeze(gModel.predict(inGEN))
     aggWidth = int(math.sqrt(num_images))
@@ -225,53 +241,54 @@ def generate_and_save(gModel, dModel, ganModel, num_images, prefix):
         osp.join(_outDirPath, '{}.png'.format(prefix)))
 
 
-def fit_gan(gModel, dModel, ganModel, in_data, batch_size, num_epochs,
+def fit_gan(gModels, dModel, ganModels, in_data, batch_size, num_epochs,
             shuffle=True):
     tmpInd = np.array(range(int(np.ceil(float(in_data.shape[0])/batch_size))))
     batch_start_ind = tmpInd * batch_size
     batch_end_ind = \
         np.clip((tmpInd + 1) * batch_size, a_min=None, a_max=in_data.shape[0])
     lossHistory = {'e': [], 'b': [], 'G': [], 'D': []}
-    for eCounter in xrange(num_epochs):
+    for eC in xrange(num_epochs):
         if shuffle:
             in_data = in_data[np.random.permutation(in_data.shape[0]), :]
-        for bCounter in range(len(batch_start_ind)):
+        for bC in range(len(batch_start_ind)):
             step_time = time.time()
-            bSI, bEI = batch_start_ind[bCounter], batch_end_ind[bCounter]
-            dLoss, gLoss, dTime, gTime = \
-                train_gan_on_batch(gModel=gModel,
+            bSI, bEI = batch_start_ind[bC], batch_end_ind[bC]
+            dLoss, gLosses, dTime, gTimes = \
+                train_gan_on_batch(gModels=gModels,
                                    dModel=dModel,
-                                   ganModel=ganModel,
+                                   ganModels=ganModels,
                                    inReal=in_data[bSI:bEI, :])
-            lossHistory['e'].append(eCounter)
-            lossHistory['b'].append(bCounter)
-            lossHistory['G'].append(gLoss)
+            lossHistory['e'].append(eC)
+            lossHistory['b'].append(bC)
+            lossHistory['G'].append(gLosses)
             lossHistory['D'].append(dLoss)
             st = time.time()
+            if bC % 100 == 0:
+                for x in range(len(gModels)):
+                    generate_and_save(gModels[x], 1024,
+                                      '{:02d}_{:03d}-{:03d}'.format(x, eC, bC))
             imgTime = time.time() - st
-            print '... [{cS:0{fZ}d}/{tS}]) '\
-                'GLoss: {gL:018.15f} ({gT:018.15f}), '\
-                'DLoss: {dL:018.15f} ({dT:018.15f}), Saving {imgT:018.15f}, '\
-                'Step: {stepT:018.15f}'\
+            print '... [{cS:0{fZ}d}/{tS}]) Step: {stepT:018.15f}'\
                 .format(fZ=len(str(len(batch_start_ind))),
-                        cS=bCounter, tS=len(batch_start_ind),
-                        gL=gLoss, dL=dLoss, gT=gTime, dT=dTime, imgT=imgTime,
-                        stepT=time.time()-step_time)
-            generate_and_save(gModel, dModel, ganModel, 1024,
-                              '{:05d}-{:05d}'.format(eCounter, bCounter))
-        print 'E {cE:0{fZ}}/{tE}) '\
-            'GLoss: {gL:018.15f}, DLoss: {dL:018.15f}'\
+                        cS=bC, tS=len(batch_start_ind),
+                        gL=gLosses, dL=dLoss, gT=gTimes, dT=dTime,
+                        imgT=imgTime, stepT=time.time()-step_time)
+        print 'E {cE:0{fZ}}/{tE}) /*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/'\
             .format(fZ=len(str(num_epochs)),
-                    cE=eCounter, tE=num_epochs,
-                    gL=gLoss, dL=dLoss)
+                    cE=eC, tE=num_epochs, gL=gLosses, dL=dLoss)
 
 
 if __name__ == '__main__':
     np.random.seed(123)
     (trainX, trainY), (testX, testY) = load_data()
-    GEN = build_generator()
+    GENs = [build_generator() for x in range(NUM_GENERATORS)]
     DSC = build_discriminator()
-    GAN = build_gan(GEN, DSC)
-    fit_gan(GEN, DSC, GAN, trainX, BATCH_SIZE, NUM_EPOCHS, shuffle=True)
-    #print train_disc_on_batch(GEN, DSC, GAN, trainX[:BATCH_SIZE, :])
-    #print train_gen_on_batch(GEN, DSC, GAN, BATCH_SIZE)
+    GANs = [build_gan(x, DSC) for x in GENs]
+    fit_gan(gModels=GENs,
+            dModel=DSC,
+            ganModels=GANs,
+            in_data=trainX,
+            batch_size=BATCH_SIZE,
+            num_epochs=NUM_EPOCHS,
+            shuffle=True)
